b7bc23ed7081e1a19947c35bc73ebdaa8f5f3e16
bradneuberg <bradneuberg@4490c68e-677f-11de-b8ef-473237edd27e>
2009-06-12 00:31:33 +0000

    Fixes Issue 115

--- branches/brad-merge-to-trunk/src/svg.js
+++ branches/brad-merge-to-trunk/src/svg.js
@@ -6142,6 +6142,7 @@ extend(_SVGObject, {
       // snip off any filename after a final slash
       results = pathname.replace(/\/([^/]*)$/, '/');
     }
+    console.log('results='+results);
 
     return results;
   },
